7fc6045660a00515eb6cdbb47283e8cd908c588b
tanbirbd <tanbirsylhet20@gmail.com>
2021-01-20 19:06:01 +0600

    created a simple nuxt app

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>
+      Welcome to the best corny dad jokes
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Welcome to dad jokes',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'best place for corny dad jokes'
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/jokes/_id/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <nuxt to="/jokes">
+      Back to Jokes
+    </nuxt>
+    <h2>{{ joke }}</h2>
+    <hr>
+    <small>Joke ID: {{ $route.params.id }}</small>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      joke: {}
+    }
+  },
+
+  async created () {
+    const config = {
+      headers: {
+        Accept: 'application/json'
+      }
+    }
+
+    try {
+      const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config)
+
+      this.joke = res.data.joke
+      console.log(this.joke)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+</script>
+<style></style>
